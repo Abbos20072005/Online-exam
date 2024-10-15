97879cc2daefc2285e24ea31d063fe48ab6a89c7
Abbosbek <161742244+Abbos20072005@users.noreply.github.com>
2024-10-15 04:25:47 -0400

    Update models.py

--- exam/models.py
+++ exam/models.py
@@ -8,6 +8,23 @@ QUESTION_TYPE = (
     (2, 'Close'),
 )
 
+class UserAnswer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_option = models.ForeignKey(Option, null=True, blank=True, on_delete=models.SET_NULL)  # For MCQ
+    open_answer = models.TextField(null=True, blank=True)  # For open-ended questions
+    is_correct = models.BooleanField(default=False
+
+   def save(self, *args, **kwargs):
+        if self.question.question_type == 'MCQ':
+            # Auto-grade MCQ
+            self.is_correct = self.selected_option == self.question.correct_option
+        super().save(*args, **kwargs)
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='options')
+    text = models.CharField(max_length=255)
+
 
 class Exam(BaseModel):
     title = models.CharField(max_length=100)
@@ -49,6 +66,7 @@ class Question(BaseModel):
     question = models.CharField(max_length=500)
     type = models.PositiveIntegerField(choices=QUESTION_TYPE, default=1)
     is_active = models.BooleanField(default=True)
+  correct_option = models.ForeignKey('Option', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.subject.name
